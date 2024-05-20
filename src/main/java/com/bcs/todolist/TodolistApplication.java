33887f4f3ea1e014a4a0b606a8134270e66e8e6f
Andrei <inandrei@gmail.com>
2024-05-20 19:16:49 +0300

    овис

--- src/main/java/com/bcs/todolist/TodolistApplication.java
+++ src/main/java/com/bcs/todolist/TodolistApplication.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.*;
 import java.util.List;
 
 @SpringBootApplication
@@ -15,7 +16,7 @@ public class TodolistApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
 
-		RoleService roleService = new RoleService(new FileProcessorService(new ObjectMapper()));
+//		RoleService roleService = new RoleService(new FileProcessorService(new ObjectMapper()));
 //		List<Role> roles = roleService.getAllRoles();
 //		System.out.println(roles);
 //
@@ -25,7 +26,38 @@ public class TodolistApplication {
 //		Role role2 = new Role(3, "moderator");
 //		roleService.saveRole(role2);
 
-		roleService.deleteRole(3);
+//		roleService.deleteRole(3);
+
+
+		String JDBC_URL="jdbc:postgresql://localhost:5432/todolist";
+
+		// try-with-resources, opens and closes resources
+		try (Connection conn = DriverManager.getConnection(JDBC_URL, "postgres", "admin");
+			 Statement stmt = conn.createStatement();
+			 // '1' OR '1' = '1'; DROP DATABASE
+			 PreparedStatement preparedStmt = conn.prepareStatement("SELECT * FROM role WHERE id = ?");
+		) {
+			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS role (id SERIAL PRIMARY KEY,	name VARCHAR(255))");
+			System.out.println("Role table created");
+
+			stmt.executeUpdate("INSERT INTO role (name) VALUES ('admin')");
+
+			ResultSet rs = stmt.executeQuery("SELECT * FROM role");
+			while(rs.next()) {
+				System.out.println(rs.getLong("id") + ": " + rs.getString("name"));
+			}
+
+			preparedStmt.setInt(1, 2);
+			ResultSet preparedRs = preparedStmt.executeQuery();
+			while(preparedRs.next()) {
+				System.out.println("Prepared stmt - " + preparedRs.getLong("id") + ": " + preparedRs.getString("name"));
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+
+
 	}
 
 }
