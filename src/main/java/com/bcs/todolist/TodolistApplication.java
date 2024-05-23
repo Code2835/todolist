99b4f9f4e714a5c25dd58dafb688470f5c28d9e8
Andrei <inandrei@gmail.com>
2024-05-23 18:13:04 +0300

    start

--- src/main/java/com/bcs/todolist/TodolistApplication.java
+++ src/main/java/com/bcs/todolist/TodolistApplication.java
@@ -16,6 +16,9 @@ public class TodolistApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
 
+
+
+
 //		RoleService roleService = new RoleService(new FileProcessorService(new ObjectMapper()));
 //		List<Role> roles = roleService.getAllRoles();
 //		System.out.println(roles);
@@ -29,33 +32,33 @@ public class TodolistApplication {
 //		roleService.deleteRole(3);
 
 
-		String JDBC_URL="jdbc:postgresql://localhost:5432/todolist";
-
-		// try-with-resources, opens and closes resources
-		try (Connection conn = DriverManager.getConnection(JDBC_URL, "postgres", "admin");
-			 Statement stmt = conn.createStatement();
-			 // '1' OR '1' = '1'; DROP DATABASE
-			 PreparedStatement preparedStmt = conn.prepareStatement("SELECT * FROM role WHERE id = ?");
-		) {
-			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS role (id SERIAL PRIMARY KEY,	name VARCHAR(255))");
-			System.out.println("Role table created");
-
-			stmt.executeUpdate("INSERT INTO role (name) VALUES ('admin')");
-
-			ResultSet rs = stmt.executeQuery("SELECT * FROM role");
-			while(rs.next()) {
-				System.out.println(rs.getLong("id") + ": " + rs.getString("name"));
-			}
-
-			preparedStmt.setInt(1, 2);
-			ResultSet preparedRs = preparedStmt.executeQuery();
-			while(preparedRs.next()) {
-				System.out.println("Prepared stmt - " + preparedRs.getLong("id") + ": " + preparedRs.getString("name"));
-			}
-		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
+//		String JDBC_URL="jdbc:postgresql://localhost:5432/todolist";
+//
+//		// try-with-resources, opens and closes resources
+//		try (Connection conn = DriverManager.getConnection(JDBC_URL, "postgres", "admin");
+//			 Statement stmt = conn.createStatement();
+//			 // '1' OR '1' = '1'; DROP DATABASE
+//			 PreparedStatement preparedStmt = conn.prepareStatement("SELECT * FROM role WHERE id = ?");
+//		) {
+//			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS role (id SERIAL PRIMARY KEY,	name VARCHAR(255))");
+//			System.out.println("Role table created");
+//
+//			stmt.executeUpdate("INSERT INTO role (name) VALUES ('admin')");
+//
+//			ResultSet rs = stmt.executeQuery("SELECT * FROM role");
+//			while(rs.next()) {
+//				System.out.println(rs.getLong("id") + ": " + rs.getString("name"));
+//			}
+//
+//			preparedStmt.setInt(1, 2);
+//			ResultSet preparedRs = preparedStmt.executeQuery();
+//			while(preparedRs.next()) {
+//				System.out.println("Prepared stmt - " + preparedRs.getLong("id") + ": " + preparedRs.getString("name"));
+//			}
+//		}
+//		catch(Exception e){
+//			e.printStackTrace();
+//		}
 
 
 	}
